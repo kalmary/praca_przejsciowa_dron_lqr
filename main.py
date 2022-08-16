2cf971f70cf2a5851f13712884b11b8c61aa8156
michalsiniarski <siniarskim@gmail.com>
2022-08-16 18:40:30 +0200

    drone0.5 nie ma wykresow

--- main.py
+++ main.py
@@ -86,6 +86,7 @@ def RHS(x, t, u_control):
 
     rho_0 = 1.225
     rho = rho_0 * math.pow((1.0 - math.fabs(x[4]) / 44300.0), 4.256)
+
     # exit()
     Q_dyn = 0.5 * rho * V * V
     L = 0.
@@ -125,7 +126,6 @@ def RHS(x, t, u_control):
     if n == 8:
         dx_dt[6] = (1.0 / Tau) * (-x[6] + Th * u_control[0])
         dx_dt[7] = (1.0 / Tau) * (-x[7] + Th * u_control[1])
-    print(dx_dt, 1)
     return dx_dt
 
 
@@ -172,6 +172,9 @@ def trajectory(X, Vel, dt):
     return Z, alpha
 
 
+#
+# ----------------------------------------------------------
+#
 def getQR(n, m):
     Q = np.diag(np.diag(np.ones((n, n))))
     R = np.diag(np.diag(np.ones((m, m))))
@@ -189,6 +192,9 @@ def getQR(n, m):
     return Q, R
 
 
+#
+# ----------------------------------------------------------
+#
 def keep_my_size(vector):
     vector2 = declare_vector(len(vector))
 
@@ -196,6 +202,23 @@ def keep_my_size(vector):
         vector2[i] = vector[i]
 
     return vector2
+
+
+#
+# ----------------------------------------------------------
+#
+def mdl(X, Y, theta, c):
+    xs0 = np.array([-0.1, 0.1, 0.1, -0.1, -0.1]) * c
+    ys0 = np.array([-0.1, 0.1, 0.1, -0.1, -0.1]) * c
+
+    xs = declare_vector(5)
+    ys = declare_vector(5)
+
+    for i in range(0, 5):
+        xs[i] = X + xs0[i] * math.cos(theta) - ys0[i] * math.sin(theta)
+        ys[i] = Y + xs0[i] * math.sin(theta) - ys0[i] * math.cos(theta)
+
+    return xs, ys
 
 
 #
@@ -220,9 +243,42 @@ def main():
     dt = 0.01
     t_pom = int((t_end + dt) / dt)
 
+    # -------------------PLOT----------------------
+    plt.figure(figsize=(15, 7))
+    plt.ion()
+    ax = plt.gca()
+    plt.axis([0., 10., 0, 8])
+
+    # lines
+    line0 = ax.plot([], [], 'r')
+    line1 = ax.plot([], [], 'b')
+    line2 = ax.plot([], [], 'g')
+
+    # points
+    pnt0 = ax.plot([], [], 'or')
+    pnt1 = ax.plot([], [], 'ob')
+    pnt2 = ax.plot([], [], 'og')
+
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, labels)
+    # -------------------PLOT----------------------
+
     tp = []
-    yp = np.zeros((len(x), t_pom))
-    up = np.zeros((len(u_control), t_pom))
+
+    # yp = np.zeros((len(x), t_pom))
+    yp0 = []
+    yp1 = []
+    yp2 = []
+    yp3 = []
+    yp4 = []
+    yp5 = []
+    yp6 = []
+    yp7 = []
+
+    # up = np.zeros((len(u_control), t_pom))
+    up0 = []
+    up1 = []
+
     gp = []
     zp = []
 
@@ -240,16 +296,43 @@ def main():
         z_ref = z_terr + h_flight
 
         tp.append(t)
-        yp[:, i] = np.transpose(x)
-        up[:, i] = np.transpose(u_control)
+
+        # yp[:, i] = np.transpose(x)
+        yp0.append(x[0])
+        yp1.append(x[1])
+        yp2.append(x[2])
+        yp3.append(x[3])
+        yp4.append(x[4])
+        yp5.append(x[5])
+        yp6.append(x[6])
+        yp7.append(x[7])
+
+        # up[:, i] = np.transpose(u_control)
+        up0.append(u_control[0])
+        up1.append(u_control[1])
 
         gp.append(z_terr)
         zp.append(z_ref)
 
+        # -------------------PLOT----------------------
+        #
+        text = 't={:8.4f}   V = {:9f}   Vx = {:9f}    Vz = {:9f}    teta = {:9f}    alpha = {:9f}   ' \
+               '|||     u1 = {:9f}    u2 = {:9f}    ' \
+               '|||     e_v={:9f}   e(z) = {:9f}.'.format(t, V, v_x, v_z, theta, alpha * rad2deg, T1, T2, e_v, e[4])
+        #
+        line0.set_data(tp, yp[3, :])
+        line1.set_data(tp, gp[3, :])
+        line2.set_data(tp, up[3, :])
+
+        pnt0.set_data(t, );
+        pnt1.set_data(t, )
+        pnt.set_data(t, )
+        # -------------------PLOT----------------------
+
         x_ref = X
 
         Q, R = getQR(n, m)
-        print(x)
+
         # get e
         e = np.zeros((n, 1))
         e[0] = x[0] - (np.cos(x[5]) * Vx + np.sin(x[5]) * Vz)
@@ -261,13 +344,12 @@ def main():
         e[5] = x[5] - 0.
 
         A, B = Jacob_AB(RHS, x, t, u_control, n, m)
-        K_gain, _, _ = lqr2(A, B, Q, R)
-        print(x)
 
+        K_gain, _, _ = lqr2(A, B, Q, R)
 
         u_control = -K_gain @ e
         u_control = keep_my_size(u_control)
-        print(x)
+
         u_max = 10000.
         u_control[0] = np.maximum(-u_max, np.minimum(u_max, u_control[0]))
         u_control[1] = np.maximum(-u_max, np.minimum(u_max, u_control[1]))
@@ -281,13 +363,43 @@ def main():
             ax_wind = 0.0
             az_wind = 0.0  # 15.5 + 3.0 * (1.0 - 2.0 * rand()) ????
 
-        print(x, "dotad dziala")
+        i += 1
+        #WYKRES OGARNIJ
+        if i==1 or i % 5 == 0:
+
+            ax.relim()
+            ax.autoscale_view(False, True, False)
+            ax.grid(True)
+            legend([line0, line1, line2], [])
+            #v_x = Vx  # sin(x[5]) * x[0] -cos(x[5]) * x[1]
+            #v_z = Vz  # sin(x[5]) * x[0] -cos(x[5]) * x[1]
+            #V = math.sqrt(x[0] * x[0] + x[1] * x[1])
+            #e_v = Vel - V
+            #theta = x[5] * rad2deg
+            #alt = -x[4]
+            #T1 = u_control[0]
+            #T2 = u_control[1]
+            #gamma = math.atan(((math.sin(x[5]) * x[0]) - (math.cos(x[5]) * x[1])) / (
+            #            (math.cos(x[5]) * x[0]) + (math.sin(x[5]) * x[2]))) * rad2deg
+
+            #xs, zs = mdl(x[3], -x[4], x[5], 0.5)
+
+        if t > 5.0:
+            tp.pop(0)
+
+            yp0.pop(0)
+            yp1.pop(0)
+            yp2.pop(0)
+            yp3.pop(0)
+            yp4.pop(0)
+            yp5.pop(0)
+            yp6.pop(0)
+            yp7.pop(0)
+
+            up0.pop(0)
+            up1.pop(0)
 
         x = fd_rk45(RHS, x, t, dt, u_control)
-
-        i += 1
         t += dt
-
-
 if __name__ == '__main__':
     main()
